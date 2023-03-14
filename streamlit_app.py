7588df039837350c462d0ea45c191ac6a2544c52
goldengrape <github@goldengrape.org>
2023-03-13 20:31:29 -0700

    语言列表

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,22 @@ import os
 import platform 
 import subprocess
 import openai 
+from utils import lang_list
 # import io import BytesIO
 
-st.title("PDF Bilingual Translator")
+st.title("Bilingual PDF")
+st.info('''This app is used to translate PDF files into bilingual PDF files. 
+The translation is powered by OpenAI GPT-3.5 Turbo. 
+The translation is not perfect.
+And very very sloooow.
+''')
 col1,col2=st.columns(2)
 file=col1.file_uploader("Upload a PDF file", type="pdf")
 col21,col22=col2.columns([1,2])
 start_page=col21.text_input("Start Page",1)
 end_page=col21.text_input("End Page",1)
-source_language=col22.selectbox("Target Language",["English","Chinese"])
-target_language=col22.selectbox("Target Language",["Chinese","English"])
+source_language=col22.selectbox("Target Language",lang_list("english"))
+target_language=col22.selectbox("Target Language",lang_list("simplified chinese"))
 
 OPENAI_API_KEY=st.text_input("OpenAI API KEY",type="password")
 translate_button=st.button("Translate")
